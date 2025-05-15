ee203723b81beb57106377a1ba4fd8c39ecff2e2
dmolinsky <diana.molinsky@iths.se>
2025-05-15 11:37:31 +0200

    created service class with mocked object in method getOptimalWeather to test view and implement controller

---
+++ src/main/java/com/dmolinsky/weatherapp/service/WeatherService.java
@@ -0,0 +1,21 @@
+package com.dmolinsky.weatherapp.service;
+
+import com.dmolinsky.weatherapp.model.WeatherData;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class WeatherService {
+
+    public WeatherData getOptimalWeather() {
+        String origin = "MockWeatherAPI";
+        double temperature = 19.2;
+        int humidity = 42;
+        double precipitationAmount = 0.0;
+        double windSpeed = 2.5;
+        LocalDateTime timestamp = LocalDateTime.now().plusHours(24);
+
+        return new WeatherData(origin, temperature, humidity, precipitationAmount, windSpeed, timestamp);
+    }
+}
