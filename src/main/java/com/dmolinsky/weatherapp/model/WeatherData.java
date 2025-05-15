29d38f20ef6cdbc3079ace3480f76c4dd39fe7fa
dmolinsky <diana.molinsky@iths.se>
2025-05-15 13:13:05 +0200

    changed icon set to png included in project. made adjustment to model and service to handle the new icon set and updated index view

--- src/main/java/com/dmolinsky/weatherapp/model/WeatherData.java
+++ src/main/java/com/dmolinsky/weatherapp/model/WeatherData.java
@@ -1,14 +1,7 @@
 package com.dmolinsky.weatherapp.model;
 
-import lombok.*;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@ToString
-//@AllArgsConstructor
-@NoArgsConstructor
 public class WeatherData {
 
     private String origin;
@@ -17,14 +10,73 @@ public class WeatherData {
     private double precipitationChance;
     private double windSpeed;
     private LocalDateTime timestamp;
+    private String iconFilename;
 
-    public WeatherData(String origin, double temperature, int humidity, double precipitationChance, double windSpeed, LocalDateTime timestamp) {
+    public WeatherData(String origin, double temperature, int humidity,
+                       double precipitationChance, double windSpeed,
+                       LocalDateTime timestamp, String iconFilename) {
         this.origin = origin;
         this.temperature = temperature;
         this.humidity = humidity;
         this.precipitationChance = precipitationChance;
         this.windSpeed = windSpeed;
         this.timestamp = timestamp;
+        this.iconFilename = iconFilename;
     }
 
+    public String getOrigin() {
+        return origin;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public double getPrecipitationChance() {
+        return precipitationChance;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getIconFilename() {
+        return iconFilename;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setPrecipitationChance(double precipitationChance) {
+        this.precipitationChance = precipitationChance;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setIconClass(String iconFilename) {
+        this.iconFilename = iconFilename;
+    }
 }
--- src/main/java/com/dmolinsky/weatherapp/service/WeatherService.java
+++ src/main/java/com/dmolinsky/weatherapp/service/WeatherService.java
@@ -15,7 +15,24 @@ public class WeatherService {
         double precipitationChance = 0.0;
         double windSpeed = 2.5;
         LocalDateTime timestamp = LocalDateTime.now().plusHours(24);
+        String iconFilename = mapSymbolToIconFile(1);
 
-        return new WeatherData(origin, temperature, humidity, precipitationChance, windSpeed, timestamp);
+        return new WeatherData(origin, temperature, humidity, precipitationChance, windSpeed, timestamp, iconFilename);
+    }
+
+    private String mapSymbolToIconFile(int symbol) {
+        return switch (symbol) {
+            case 1 -> "day-sunny.png";
+            case 2 -> "day-cloudy.png";
+            case 3, 4, 5 -> "cloudy.png";
+            case 6 -> "fog.png";
+            case 7 -> "showers.png";
+            case 8, 9 -> "rain.png";
+            case 10 -> "sleet.png";
+            case 11, 12 -> "snow.png";
+            case 13, 15 -> "thunderstorm.png";
+            case 14 -> "storm-showers.png";
+            default -> null;
+        };
     }
 }
